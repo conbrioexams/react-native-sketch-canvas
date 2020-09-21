4a85ce76e8a8b5b6b65b1e877ed1e4d9e70c9879
Zheng How <zhenghow0214@gmail.com>
2020-09-21 17:58:52 +0800

    - fixed android crash on rendering large-dimension background pictures by setting inSampleSize to size down the pic.

--- android/src/main/java/com/terrylinla/rnsketchcanvas/SketchCanvas.java
+++ android/src/main/java/com/terrylinla/rnsketchcanvas/SketchCanvas.java
@@ -65,17 +65,36 @@ public class SketchCanvas extends View {
     public boolean openImageFile(String filename, String directory, String mode) {
         if(filename != null) {
             int res = mContext.getResources().getIdentifier(
-                filename.lastIndexOf('.') == -1 ? filename : filename.substring(0, filename.lastIndexOf('.')), 
-                "drawable", 
+                filename.lastIndexOf('.') == -1 ? filename : filename.substring(0, filename.lastIndexOf('.')),
+                "drawable",
                 mContext.getPackageName());
             BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-            Bitmap bitmap = res == 0 ? 
+            Bitmap bitmap = res == 0 ?
                 BitmapFactory.decodeFile(new File(filename, directory == null ? "" : directory).toString(), bitmapOptions) :
                 BitmapFactory.decodeResource(mContext.getResources(), res);
             if(bitmap != null) {
                 mBackgroundImage = bitmap;
                 mOriginalHeight = bitmap.getHeight();
                 mOriginalWidth = bitmap.getWidth();
+
+                int inSampleSize = Utility.calculateInSampleSize(
+                    mOriginalWidth,
+                    mOriginalHeight,
+                    2048, /* setting 2048 as default height and width for now */
+                    2048
+                );
+
+                if (inSampleSize > 1) {
+                    bitmapOptions.inSampleSize = inSampleSize;
+                    bitmap = res == 0 ?
+                        BitmapFactory.decodeFile(new File(filename, directory == null ? "" : directory).toString(), bitmapOptions) :
+                        BitmapFactory.decodeResource(mContext.getResources(), res);
+
+                    mBackgroundImage = bitmap;
+                    mOriginalHeight = bitmap.getHeight();
+                    mOriginalWidth = bitmap.getWidth();
+                }
+
                 mContentMode = mode;
 
                 invalidateCanvas(true);
@@ -311,7 +330,7 @@ public class SketchCanvas extends View {
             mTranslucentDrawingBitmap = Bitmap.createBitmap(getWidth(), getHeight(),
                     Bitmap.Config.ARGB_8888);
             mTranslucentDrawingCanvas = new Canvas(mTranslucentDrawingBitmap);
-            
+
             for(CanvasText text: mArrCanvasText) {
                 PointF position = new PointF(text.position.x, text.position.y);
                 if (!text.isAbsoluteCoordinate) {
@@ -347,8 +366,8 @@ public class SketchCanvas extends View {
         if (mBackgroundImage != null) {
             Rect dstRect = new Rect();
             canvas.getClipBounds(dstRect);
-            canvas.drawBitmap(mBackgroundImage, null, 
-                Utility.fillImage(mBackgroundImage.getWidth(), mBackgroundImage.getHeight(), dstRect.width(), dstRect.height(), mContentMode), 
+            canvas.drawBitmap(mBackgroundImage, null,
+                Utility.fillImage(mBackgroundImage.getWidth(), mBackgroundImage.getHeight(), dstRect.width(), dstRect.height(), mContentMode),
                 null);
         }
 
@@ -384,14 +403,14 @@ public class SketchCanvas extends View {
     private Bitmap createImage(boolean transparent, boolean includeImage, boolean includeText, boolean cropToImageSize) {
         Bitmap bitmap = Bitmap.createBitmap(
             mBackgroundImage != null && cropToImageSize ? mOriginalWidth : getWidth(),
-            mBackgroundImage != null && cropToImageSize ? mOriginalHeight : getHeight(), 
+            mBackgroundImage != null && cropToImageSize ? mOriginalHeight : getHeight(),
             Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         canvas.drawARGB(transparent ? 0 : 255, 255, 255, 255);
 
         if (mBackgroundImage != null && includeImage) {
             Rect targetRect = new Rect();
-            Utility.fillImage(mBackgroundImage.getWidth(), mBackgroundImage.getHeight(), 
+            Utility.fillImage(mBackgroundImage.getWidth(), mBackgroundImage.getHeight(),
                 bitmap.getWidth(), bitmap.getHeight(), "AspectFit").roundOut(targetRect);
             canvas.drawBitmap(mBackgroundImage, null, targetRect, null);
         }
@@ -404,7 +423,7 @@ public class SketchCanvas extends View {
 
         if (mBackgroundImage != null && cropToImageSize) {
             Rect targetRect = new Rect();
-            Utility.fillImage(mDrawingBitmap.getWidth(), mDrawingBitmap.getHeight(), 
+            Utility.fillImage(mDrawingBitmap.getWidth(), mDrawingBitmap.getHeight(),
                 bitmap.getWidth(), bitmap.getHeight(), "AspectFill").roundOut(targetRect);
             canvas.drawBitmap(mDrawingBitmap, null, targetRect, mPaint);
         } else {
--- android/src/main/java/com/terrylinla/rnsketchcanvas/Utility.java
+++ android/src/main/java/com/terrylinla/rnsketchcanvas/Utility.java
@@ -11,19 +11,39 @@ public final class Utility {
             case "AspectFill": {
                 float scaleFactor = targetAspectRatio < imageAspectRatio ? targetHeight / imgHeight : targetWidth / imgWidth;
                 float w = imgWidth * scaleFactor, h = imgHeight * scaleFactor;
-                return new RectF((targetWidth - w) / 2, (targetHeight - h) / 2, 
+                return new RectF((targetWidth - w) / 2, (targetHeight - h) / 2,
                     w + (targetWidth - w) / 2, h + (targetHeight - h) / 2);
             }
             case "AspectFit":
             default: {
                 float scaleFactor = targetAspectRatio > imageAspectRatio ? targetHeight / imgHeight : targetWidth / imgWidth;
                 float w = imgWidth * scaleFactor, h = imgHeight * scaleFactor;
-                return new RectF((targetWidth - w) / 2, (targetHeight - h) / 2, 
+                return new RectF((targetWidth - w) / 2, (targetHeight - h) / 2,
                     w + (targetWidth - w) / 2, h + (targetHeight - h) / 2);
             }
             case "ScaleToFill": {
                 return  new RectF(0, 0, targetWidth, targetHeight);
             }
         }
+    }
+
+    public static int calculateInSampleSize(
+        int bitmapWidth,
+        int bitmapHeight,
+        int canvasWidth,
+        int canvasHeight) {
+        // Raw height and width of image
+        final int height = bitmapHeight;
+        final int width = bitmapWidth;
+        int inSampleSize = 1;
+
+        if (height > canvasHeight || width > canvasWidth) {
+            while ((bitmapHeight / inSampleSize) >= (canvasHeight * 1.5)
+                    && (bitmapWidth / inSampleSize) >= canvasWidth * 1.5) {
+                inSampleSize += 1;
+            }
+        }
+
+        return inSampleSize;
     }
 }
